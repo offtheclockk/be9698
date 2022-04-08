46398fc719added7f23d71b6974e838a6c609e2b
offtheclockk <tristan@offtheclockk.com>
2022-04-08 21:43:04 +0000

    update

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -21,5 +21,6 @@ export const theme = createMuiTheme({
     primary: { main: "#3A8DFF" },
     secondary: { main: "#B0B0B0" },
     gradient: {main: "#86B9FF"}
-  }
+  },
+  spacing: [0, 2, 6, 15, 20, 48],
 });
